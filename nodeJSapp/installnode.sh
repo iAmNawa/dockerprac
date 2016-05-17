22a59ae4d68e3de968fa28345d06ae170a4d1aea
Paul Borawski <paulborawski14@gmail.com>
2016-05-17 15:35:45 -0700

    ubuntu setup

---
+++ nodeJSapp/installnode.sh
@@ -0,0 +1,15 @@
+Inside installnode.sh, there should be these commands
+
+Start a docker container with ubuntu:
+docker run -t -i ubuntu:14.04 /bin/bash
+
+Immediately following:
+apt-get update
+apt-get upgrade
+apt-get install nano ORRRRR vim
+apt-get install vim
+
+apt-get update
+apt-get install --yes nodejs
+apt-get install --yes nodejs-legacy
+apt-get install --yes npm
